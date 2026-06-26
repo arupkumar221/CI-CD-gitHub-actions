937f0c2484ed13d9b28aff2843f16d2ec6443d19
ARUP KUMAR DASH <arupkumard31@gmail.com>
2026-06-26 11:47:45 +0530

    Configure S3 backend and update EC2 instance tags

--- main.tf
+++ main.tf
@@ -1,12 +1,20 @@
+terraform {
+  backend "s3" {
+    bucket = "arup-terraform-state-bucket"
+    key    = "ec2/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
 provider "aws" {
   region = "us-east-1"
 }
 
 resource "aws_instance" "my_ec2" {
-  ami           = "ami-08f44e8eca9095668" # Replace with your AMI ID
+  ami           = "ami-08f44e8eca9095668"
   instance_type = "t2.micro"
 
   tags = {
-    Name = "GitHub-Actions-EC2"
+    Name = "GitHub-Actions-EC2-arup"
   }
 }
